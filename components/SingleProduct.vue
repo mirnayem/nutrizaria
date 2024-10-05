ac17fb6ac12437727cec1a0020392a64e375694d
mirnayem <mirnayem49@gmail.com>
2024-10-05 11:26:14 +0600

    image size improved

--- components/SingleProduct.vue
+++ components/SingleProduct.vue
@@ -16,16 +16,10 @@ const props = defineProps<Props>();
 <template>
   <div class="product__single grid gap-1 relative group">
     <div class="image transition-all duration-700 ease-in-out w-fit mx-auto">
-      <NuxtImg
+      <img
         :src="`/images/${product.image}`"
         :alt="product.name"
-        sizes="(max-width: 640px) 256px, (max-width: 768px) 288px, (max-width: 1024px) 320px, 350px"
-        :srcset="`/images/${product.image}?w=256 256w, /images/${product.image}?w=288 288w, /images/${product.image}?w=320 320w, /images/${product.image}?w=350 350w`"
-        width="350"
-        height="350"
-        loading="lazy"
-        format="webp"
-        class="rounded-md sm:w-64 sm:h-64 w-72 h-72 object-cover"
+        class="rounded-md sm:w-72 sm:h-72 w-80 h-80 object-cover"
       />
     </div>
 
